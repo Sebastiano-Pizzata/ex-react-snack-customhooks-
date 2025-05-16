4c8884c6fa5318cc42b5bf11e79d429d983ca470
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-05-16 14:35:12 +0200

    ADD: Snack 1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import useSwitch from "./assets/useSwitch";
+import useDate from "./assets/useDate";
+
+function App() {
+  // const [isOn, toggle] = useSwitch();
+  const currentDate = useDate();
+
+  return (
+    // <div>
+    //   <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+    //   <button onClick={toggle}>Cambia Stato</button>
+    // </div>
+    <div>
+      <h1>Data e ora attuali:</h1>
+      <p>{currentDate.toLocaleString()}</p>
+    </div>
+  );
+}
+
+export default App;
