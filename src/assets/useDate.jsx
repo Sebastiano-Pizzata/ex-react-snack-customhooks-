7d4f33dc9d5ce32097e2b2c5b4fb1ba7391e498a
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-05-16 14:43:12 +0200

    ADD: Snack 2

--- src/assets/useDate.jsx
+++ src/assets/useDate.jsx
@@ -1,3 +1,15 @@
-export default function useDate() {
+import { useState, useEffect } from 'react'
 
+export default function useDate() {
+    const [currDate, setCurrDate] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrDate(new Date())
+        }, 1000)
+
+        return () => clearInterval(interval)
+    }, [])
+
+    return currDate
 }
